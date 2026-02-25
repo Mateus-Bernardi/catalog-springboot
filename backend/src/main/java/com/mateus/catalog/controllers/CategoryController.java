5361cdc4badf3e33b7f6e07906a6c29d612b4e92
Mateus-Bernardi <mateusbnalves@gmail.com>
2026-02-25 12:20:29 -0300

    feat(category): implement pagination for findAll method

--- backend/src/main/java/com/mateus/catalog/controllers/CategoryController.java
+++ backend/src/main/java/com/mateus/catalog/controllers/CategoryController.java
@@ -3,13 +3,14 @@ package com.mateus.catalog.controllers;
 import com.mateus.catalog.dto.CategoryDTO;
 import com.mateus.catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -19,8 +20,13 @@ public class CategoryController {
     public CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+                                                     @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+                                                     @RequestParam(value = "direction", defaultValue = "ASC") String direction)
+    {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
