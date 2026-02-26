671ab4240a87e724ac84c07f4cd44486cd9a6034
Mateus-Bernardi <mateusbnalves@gmail.com>
2026-02-26 16:35:25 -0300

    feat(tests): add ProductRepositoryTests and Factory class

---
+++ backend/src/test/java/com/mateus/catalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,64 @@
+package com.mateus.catalog.repositories;
+
+import com.mateus.catalog.entities.Product;
+import com.mateus.catalog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setup() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+        product = repository.save(product);
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        repository.deleteById(existingId);
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldNotThrowExceptionWhenIdDoesNotExist() {
+        Assertions.assertDoesNotThrow(() -> {
+            repository.deleteById(nonExistingId);
+        });
+    }
+
+    @Test
+    public void findByIdShouldReturnNonEmptyOptionalWhenIdExists() {
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void findByIdShouldReturnEmptyOptionalWhenIdDoesNotExist() {
+        Optional<Product> result = repository.findById(nonExistingId);
+        Assertions.assertTrue(result.isEmpty());
+    }
+}
---
+++ backend/src/test/java/com/mateus/catalog/tests/Factory.java
@@ -0,0 +1,20 @@
+package com.mateus.catalog.tests;
+
+import com.mateus.catalog.dto.ProductDTO;
+import com.mateus.catalog.entities.Category;
+import com.mateus.catalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+    public static Product createProduct() {
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+        product.getCategories().add(new Category(1L, "Electronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
