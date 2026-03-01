33206338a326f64499df59814a6b3552aa7586f7
Mateus-Bernardi <mateusbnalves@gmail.com>
2026-03-01 11:12:49 -0300

    feat(validation): add validation error handling and classes

---
+++ backend/src/main/java/com/mateus/catalog/exceptions/ValidationError.java
@@ -0,0 +1,19 @@
+package com.mateus.catalog.exceptions;
+
+import com.mateus.catalog.controllers.exceptions.StandardError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
